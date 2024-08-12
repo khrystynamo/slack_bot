34c14d8be2569cd20c86bae90b9a3bd6c9f45ae2
Christina Mosiichuk <kris161201@gmail.com>
2024-08-12 21:15:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+1. [Slack API: Getting Started](https://api.slack.com/start)
+2. [Slack API: Bolt for Python](https://slack.dev/bolt-python/tutorial/getting-started)
+3. [Slack API: Events API](https://api.slack.com/apis/connections/events-api)
+4. [Slack API: Socket Mode](https://api.slack.com/apis/connections/socket)
+5. [Real Python: Building a Slack Bot Using Python and Flask](https://realpython.com/python-slack-bot/)
+6. [Medium: How to Build a Slack Bot with Python](https://medium.com/python-in-plain-english/how-to-build-a-slack-bot-with-python-ef9496b61e46)
+7. [FreeCodeCamp: Build Your First Slack Bot](https://www.freecodecamp.org/news/build-your-first-slack-bot-with-python/)
+8. [Google Cloud: Using OAuth 2.0 for Server to Server Applications](https://developers.google.com/identity/protocols/oauth2/service-account)
+9. [Gspread Documentation](https://gspread.readthedocs.io/en/latest/)
+10. [Python Quickstart | Sheets API](https://developers.google.com/sheets/api/quickstart/python)
+11. [GitHub: Slack Developer Tools](https://github.com/slackapi)
+'''
